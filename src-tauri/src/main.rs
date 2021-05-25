52cfdafdec1c139b9b639b01332732381c59f84f
Yi Liu <glistenlau@gmail.com>
2021-05-24 22:13:24 -0700

    add menus

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, MenuItem};
+
 mod core;
 mod entity;
 mod graphql;
@@ -16,7 +18,49 @@ fn main() {
         Err(e) => log::error!("logger setup failed: {}", e),
     }
 
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let close = CustomMenuItem::new("close".to_string(), "Close");
+    let menu = vec![
+        // on macOS first menu is always app name
+        Menu::new(
+            "AP Database Dev Tool",
+            vec![
+                MenuItem::Services,
+                MenuItem::Separator,
+                MenuItem::Hide,
+                MenuItem::HideOthers,
+                MenuItem::ShowAll,
+                MenuItem::Separator,
+                MenuItem::Custom(quit),
+                MenuItem::Custom(close),
+            ],
+        ),
+        Menu::new(
+            "Edit",
+            vec![
+                MenuItem::Undo,
+                MenuItem::Redo,
+                MenuItem::Separator,
+                MenuItem::Cut,
+                MenuItem::Copy,
+                MenuItem::Paste,
+                MenuItem::Separator,
+                MenuItem::SelectAll,
+            ],
+        ),
+    ];
+
     tauri::Builder::default()
+        .menu(menu)
+        .on_menu_event(|event| match event.menu_item_id().as_str() {
+            "quit" => {
+                std::process::exit(0);
+            }
+            "close" => {
+                event.window().close().unwrap();
+            }
+            _ => {}
+        })
         // This is where you pass in your commands
         .invoke_handler(tauri::generate_handler![handlers::invoke_handler])
         .run(tauri::generate_context!())
