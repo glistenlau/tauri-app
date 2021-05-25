1555a1358b364649fd1cb5c28532f7ad928e8cfa
Yi Liu <glistenlau@gmail.com>
2021-05-24 23:05:34 -0700

    fix handler

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -18,9 +18,11 @@ export const requestAsync = async (
   payload: any
 ) => {
   const args = {
+    handler: {
       name: handlerName,
       action,
       payload
+    }
   };
 
   const res = JSON.parse(await invoke("invoke_handler", args));
